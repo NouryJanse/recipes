58029d635b2c97d19745dce71601f6e726e8729b
Noury Janse <hello@nouryjanse.nl>
2021-11-28 22:19:04 +0100

    My first button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,19 @@
+import styled from 'styled-components';
 
-const Button = () => {
+const ButtonStyle = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: grey;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
+const Button = (props) => {
+    const { content, onClick } = props;
+
+    return <ButtonStyle onClick={onClick}>{content}</ButtonStyle>
 }
 
 export default Button;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,4 @@
-import styled from 'styled-components';
-
-const StyledRecipe = styled.div`
-    width: 100px;
-    height: 100px;
-    background-color: grey;
-    color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
+import Button from './Button';
 
 const logSomething = () =>  {
     console.log(`Recipe ${Date.now()}`);
@@ -16,10 +6,12 @@ const logSomething = () =>  {
 
 export default function Recipe(data) {
     const recipe = data.recipe;
+    const buttonVal = `Click me and view the logs! ${recipe.title}`;
     
     return (
-        <StyledRecipe onClick={() => logSomething()}>Click me and view the logs! {recipe.title}</StyledRecipe>
+        <Button
+            content={buttonVal}
+            onClick={() => logSomething()}
+        />
     )
 }
-
-// View component here
