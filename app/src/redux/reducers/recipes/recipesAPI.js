c7b35f44ddea225f19a129c7948978c71c9cf8c0
Noury Janse <github@nouryjanse.nl>
2022-01-03 22:18:35 +0100

    Modified Redux store for user

--- app/src/redux/reducers/recipes/recipesAPI.js
+++
@@ -1,5 +0,0 @@
-// A mock function to mimic making an async request for data
-export async function fetchRecipesAPI(userID = 1) {
-    const response = await fetch('/data/recipes.json');
-    return response.json();
-}
