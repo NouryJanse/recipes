8e71b30a5a169cd6d7a78291c8f6b8fac2bb5158
Noury Janse <github@nouryjanse.nl>
2022-01-13 14:56:57 +0100

    Created a Dropdown component

---
+++ app/src/components/Form/Dropdown/index.js
@@ -0,0 +1,40 @@
+import { StyledDropdown, StyledOption, StyledLabel } from './styled';
+import { useState, useEffect } from 'react';
+
+const Dropdown = ({ label, name, validation, register, disabled, options, defaultValue }) => {
+  options = options.map((option) => {
+    return option.name === defaultValue ? { ...option, selected: true } : option;
+  });
+  return (
+    <div>
+      <StyledLabel htmlFor={name}>
+        {label}
+        <StyledDropdown
+          id={name}
+          name={name}
+          {...register(name, validation)}
+          disabled={disabled}
+          value={options.find((option) => option.selected).name}
+        >
+          {options.map((option, i) => {
+            return (
+              <StyledOption key={i} value={option.name} disabled={option.disabled}>
+                {option.title}
+              </StyledOption>
+            );
+          })}
+        </StyledDropdown>
+        <svg>
+          <use xlinkHref="#select-arrow-down"></use>
+        </svg>
+      </StyledLabel>
+      <svg className="sprites">
+        <symbol id="select-arrow-down" viewBox="0 0 10 6">
+          <polyline points="1 1 5 5 9 1"></polyline>
+        </symbol>
+      </svg>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ app/src/components/Form/Dropdown/styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const StyledDropdown = styled.select`
+  display: flex;
+  margin-bottom: 16px;
+  appearance: none;
+  padding: 7px 40px 7px 12px;
+  width: 100%;
+  /* border: 1px solid #e8eaed; */
+  border: 1px solid #0d6efd;
+  border-radius: 5px;
+  background: white;
+  color: black;
+  box-shadow: 0 1px 3px -2px #9098a9;
+  cursor: pointer;
+  font-family: inherit;
+  font-size: 16px;
+  transition: all 150ms ease;
+  transition: padding 200ms ease-in-out;
+
+  &:focus {
+    padding-left: 16px;
+  }
+`;
+
+export const StyledOption = styled.option`
+  color: ${(props) => (props.selected ? 'lightgrey' : 'black')};
+`;
+
+export const StyledLabel = styled.label`
+  margin-bottom: 8px;
+  color: black;
+  position: relative;
+`;
