c8a4bb24d7240b19981ad760ffc51116985abe94
Noury Janse <github@nouryjanse.nl>
2022-05-23 21:01:33 +0200

    Created default placeholder images

---
+++ app/src/constants/REPLACEMENT_IMAGES.ts
@@ -0,0 +1,6 @@
+export default {
+  heroBanner:
+    'https://res.cloudinary.com/dqnks1cyu/image/upload/v1653331963/recipes/pdcrvxlvbtjyfrndtgmn.jpg',
+  recipeCard:
+    'http://res.cloudinary.com/dqnks1cyu/image/upload/v1653309438/recipes/dueqxqsjcxvgcmo68gdj.jpg',
+}
