cca2de121fb75a3824b47e1297377a7e2bd3987f
Noury Janse <github@nouryjanse.nl>
2022-10-08 10:10:38 +0200

    Closed down all Heroku DB's and account and migrated all data to localhost

--- app/src/constants/REPLACEMENT_IMAGES.ts
+++ app/src/constants/REPLACEMENT_IMAGES.ts
@@ -1,7 +1,7 @@
 // these are 'mocked' images that serve as placeholders when there is no content
 export default {
   heroBanner:
-    'https://res.cloudinary.com/dqnks1cyu/image/upload/v1653331963/recipes/pdcrvxlvbtjyfrndtgmn.jpg',
+    'https://res.cloudinary.com/dqnks1cyu/image/upload/v1664962461/recipes/food_ytjrov.jpg',
   recipeCard:
-    'http://res.cloudinary.com/dqnks1cyu/image/upload/v1653309438/recipes/dueqxqsjcxvgcmo68gdj.jpg',
+    'https://res.cloudinary.com/dqnks1cyu/image/upload/v1664962512/recipes/healthy-eating-ingredients-732x549-thumbnail_y5ier5.jpg',
 }
