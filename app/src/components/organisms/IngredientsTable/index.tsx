06c759f39931d675a78d54cc573c14581f083d40
Noury Janse <github@nouryjanse.nl>
2023-01-17 20:10:11 +0100

    Code cleanup

--- app/src/components/organisms/IngredientsTable/index.tsx
+++ app/src/components/organisms/IngredientsTable/index.tsx
@@ -31,36 +31,11 @@ const columns: GridColDef[] = [
     width: 100,
     valueGetter: (params: GridValueGetterParams) => `${params.row.published ? 'Yes' : 'No'}`,
   },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (params: GridValueGetterParams) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  // },
-]
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ]
 
 const IngredientsTable: React.FC<IngredientsTableProps> = ({ ingredients }): ReactElement => {
-  // const dispatch = useDispatch()
-  // const navigate = useNavigate()
-  // const [isHovering, setIsHovering] = useState(false)
-
   return (
     <div style={{ height: 600, width: '100%' }}>
-      {/* <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[15]} /> */}
       <DataGrid rows={ingredients} columns={columns} pageSize={15} rowsPerPageOptions={[20]} />
     </div>
   )
