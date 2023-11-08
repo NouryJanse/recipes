6ddf0b166d2f519cc57beefebea1fe8a2a36b0c1
Noury Janse <github@nouryjanse.nl>
2023-11-08 17:13:03 +0100

    Vercel deployment

--- shopping-app/astro.config.mjs
+++ shopping-app/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-import nodejs from "@astrojs/node";
+// import nodejs from "@astrojs/node";
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: nodejs({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   integrations: [preact()],
 });
