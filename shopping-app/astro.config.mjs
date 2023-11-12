1bd511ac53d8a221da60d91ae2fee12336178e02
Noury Janse <github@nouryjanse.nl>
2023-11-12 15:29:12 +0100

    back to netlify

--- shopping-app/astro.config.mjs
+++ shopping-app/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-// import netlify from "@astrojs/netlify/functions";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [preact()],
   prefetch: true,
 });
