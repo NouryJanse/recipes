1d1c16b561bf0408a61a3a1b9b388cabd4b1aead
Noury Janse <github@nouryjanse.nl>
2023-11-22 16:06:03 +0100

    disabled recipes functionality

--- shopping-app/astro.config.mjs
+++ shopping-app/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-// import netlify from "@astrojs/netlify/functions";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
