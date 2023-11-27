7dff32b8b5f1b63e7f8760e614b6f911fd1509c2
Noury Janse <github@nouryjanse.nl>
2023-11-27 22:28:01 +0100

    Testing Netlify

--- shopping-app/astro.config.mjs
+++ shopping-app/astro.config.mjs
@@ -4,13 +4,15 @@ import vercel from "@astrojs/vercel/serverless";
 // import node from "@astrojs/node";
 // import netlify from "@astrojs/netlify/functions";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   // adapter: node({
   //   mode: "standalone",
   // }),
   integrations: [preact()],
-  prefetch: true,
+  prefetch: true
 });
