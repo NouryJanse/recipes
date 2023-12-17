888d7d747d32d35222cbf8f05714e0bbaf1f79a3
Noury Janse <github@nouryjanse.nl>
2023-12-17 18:03:25 +0100

    Bugfix socket not loaded but invoking

--- shopping-app/src/components/ShoppingList/useActivateSocket.ts
+++ shopping-app/src/components/ShoppingList/useActivateSocket.ts
@@ -6,27 +6,34 @@ export const useActivateSocket = () => {
   const socket = $socketClient.get();
 
   useEffect(() => {
-    socket.connect();
-    socket.on("message", (msg: any) => {
-      console.log(msg);
-    });
-    socket.on("connect", () => {
-      setIsConnected(true);
-    });
-    socket.on("disconnect", () => {
-      setIsConnected(false);
-    });
-    socket.on("onShoppingListUpdate", (data: any) => {
-      const parsedData = JSON.parse(data);
-      setShoppingList(parsedData.list);
-    });
-    return () => {
-      isConnected;
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("onShoppingListUpdate");
-    };
-  }, []);
+    if (socket) {
+      socket.connect();
+
+      socket.on("message", (msg: any) => {
+        console.log(msg);
+      });
+
+      socket.on("connect", () => {
+        setIsConnected(true);
+      });
+
+      socket.on("disconnect", () => {
+        setIsConnected(false);
+      });
+
+      socket.on("onShoppingListUpdate", (data: any) => {
+        const parsedData = JSON.parse(data);
+        setShoppingList(parsedData.list);
+      });
+
+      return () => {
+        isConnected;
+        socket.off("connect");
+        socket.off("disconnect");
+        socket.off("onShoppingListUpdate");
+      };
+    }
+  }, [socket]);
 
   return { isConnected };
 };
