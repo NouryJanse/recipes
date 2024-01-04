0c9c1e7448aabb9d13bfe00ff7b55a8b5b284b1f
Noury Janse <github@nouryjanse.nl>
2024-01-04 18:33:04 +0100

    Added package-lock.json file

--- shopping-app/astro.config.mjs
+++ shopping-app/astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 // import vercel from "@astrojs/vercel/serverless";
-// import node from "@astrojs/node";
-import netlify from "@astrojs/netlify/functions";
+import node from "@astrojs/node";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
-  // adapter: node({
-  //   mode: "standalone",
-  // }),
+  // adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [preact()],
   prefetch: true,
 });
