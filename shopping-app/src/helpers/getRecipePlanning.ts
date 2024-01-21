24b1b6184b5e5458ffa383778834716764a27c6a
Noury Janse <github@nouryjanse.nl>
2024-01-21 15:15:01 +0100

    The planning also shows a few days in the past

--- shopping-app/src/helpers/getRecipePlanning.ts
+++ shopping-app/src/helpers/getRecipePlanning.ts
@@ -1,15 +1,19 @@
 export const getRecipePlanning = () => {
   let arr = [];
 
+  for (let index = -4; index < 0; index++) {
+    arr.push({
+      date: getDays(new Date(), index).toLocaleDateString("nl-NL"),
+    });
+  }
   for (let index = 0; index < 10; index++) {
     arr.push({
-      date: getNextDays(new Date(), index).toLocaleDateString("nl-NL"),
+      date: getDays(new Date(), index).toLocaleDateString("nl-NL"),
     });
   }
   return arr;
 };
-
-const getNextDays = (currentDate = new Date(), daysToAdd = 1) => {
+const getDays = (currentDate = new Date(), daysToAdd = 1) => {
   const nextDate = new Date(currentDate);
   nextDate.setDate(currentDate.getDate() + daysToAdd);
   return nextDate;
