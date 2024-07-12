b5546e7a3a7ba3288471717b9c13732b1888419b
Noury Janse <github@nouryjanse.nl>
2024-07-12 21:36:01 +0200

    Updated all dependencies for shopping-app

--- shopping-app/astro.config.mjs
+++ shopping-app/astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-// import vercel from "@astrojs/vercel/serverless";
-// import netlify from "@astrojs/netlify/functions";
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
   integrations: [preact()],
-  prefetch: true,
-  // adapter: netlify(),
 });
